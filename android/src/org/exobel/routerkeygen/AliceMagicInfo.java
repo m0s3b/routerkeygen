ca37359acf1ded047359a68a29c0547c69dd37b0
Rui Araujo <rui.araujo@ieee.org>
2012-04-27 13:21:05 +0100

    Implemented Parcelable for AliceMagicInfo

--- android/src/org/exobel/routerkeygen/AliceMagicInfo.java
+++ android/src/org/exobel/routerkeygen/AliceMagicInfo.java
@@ -18,7 +18,10 @@
  */
 package org.exobel.routerkeygen;
 
-public class AliceMagicInfo {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class AliceMagicInfo implements Parcelable{
 	private final String alice;
 	private final int [] magic;
 	private final String serial;
@@ -42,4 +45,49 @@ public class AliceMagicInfo {
     public String getMac() {
         return mac;
     }
+    
+	public int describeContents() {
+		return 0;
+	}
+
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeInt(magic.length);
+		dest.writeIntArray(magic);
+		dest.writeInt(alice != null ? 1 : 0);
+		if (alice != null)
+			dest.writeString(alice);
+		dest.writeInt(serial != null ? 1 : 0);
+		if (serial != null)
+			dest.writeString(serial);
+		dest.writeInt(mac != null ? 1 : 0);
+		if (mac != null)
+			dest.writeString(mac);
+	}
+
+	public static final Parcelable.Creator<AliceMagicInfo> CREATOR = new Parcelable.Creator<AliceMagicInfo>() {
+		public AliceMagicInfo createFromParcel(Parcel in) {
+			return new AliceMagicInfo(in);
+		}
+
+		public AliceMagicInfo[] newArray(int size) {
+			return new AliceMagicInfo[size];
+		}
+	};
+
+	private AliceMagicInfo(Parcel in) {
+			magic = new int[in.readInt()];
+			in.readIntArray(magic);
+			if ( in.readInt() == 1 )
+				alice = in.readString();
+			else
+				alice = "";
+			if ( in.readInt() == 1 )
+				serial = in.readString();
+			else
+				serial = "";
+			if ( in.readInt() == 1 )
+				mac = in.readString();
+			else
+				mac = "";
+	}
 }
